153f65a459dc7795e5d25d87d0ab318152be509a
Kyi Minn Han <kyiminn17@gmail.com>
2024-03-22 00:13:45 +0630

    add asssss

---
+++ function/footer.php
@@ -0,0 +1,72 @@
+<section class="foot">
+    <hr>
+
+    <div class="web-info">
+
+        <div class="line1">
+        
+            <h3>CONTACT</h3>
+            <p class="address">Address:</p><br>
+            <p class="phno">Phone Number:</p>
+            <h2>Follow Us</h2>
+            <div class="footicon">
+                <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                <a href="#"><i class="fa-brands fa-google"></i></a>
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                <a href="#"><i class="fa-brands fa-youtube"></i></a>
+            </div>
+        </div>
+
+        <div class="line2">
+            <h3>ABOUT</h3>
+            <a href="home.php">
+                <p class="p1">About us</p>
+            </a>
+            <a href="term.php">
+                <p class="p3">Privacy Policy</p>
+            </a>
+            <a href="contact.php">
+                <p class="p4">Contact Us</p>
+            </a>
+            <a href="term.php">
+                <p class="p5">Terms & Conditions</p>
+            </a>
+        </div>
+
+        <div class="line3">
+            <h3>YOUR ACCOUNT</h3>
+            <a href="register.php">
+                <p class="p1">NEWS</p>
+            </a>
+            <a href="login.php">
+                <p class="p2">Sign In</p>
+            </a>
+        </div>
+
+
+        <div class="line4">
+            <h3>INSTALL APP</h3>
+            <p class="p1">Download From App Store or Google Play</p>
+            <div class="footerimage1">
+                <a href="#"><img class="img1" src="photo\ios.png"></a>
+                <a href="#"><img class="img2" src="photo\google.png"></a>
+
+            </div>
+
+            <p class="p6">Now You are in <?= $footername ?>.
+            </p>
+
+        </div>
+    </div>
+
+    <p class="p6">Copyright &copy; Batch 3. All rights reserved.
+    </p>
+
+</section>
+
+
+
+</body>
+
+</html>
---
+++ home.php
@@ -0,0 +1,23 @@
+<?php 
+$titlename = "Home";
+include("function/header.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="content"> <img src="photos/1.jpeg" alt="socialmedia">
+    <h1><a href="newapi.php" class="new-button">NEW</a></h1>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora ad perferendis esse blanditiis. Veniam magni natus, consectetur placeat quod atque ex laboriosam odio dignissimos corrupti praesentium, magnam laudantium quia harum!</p>    
+
+</div>
+</body>
+</html>
+<?php
+$footername = "Home page";
+include ("function/footer.php");
+?>
